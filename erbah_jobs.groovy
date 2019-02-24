b471388ef3940a881be0619cb207ebbc6790e744
fahedouch <fahedreussite@gmail.com>
2019-02-24 22:35:35 +0100

    Update erbah_jobs.groovy

--- erbah_jobs.groovy
+++ erbah_jobs.groovy
@@ -1,5 +1,4 @@
-import erbah_config.groovy
-
+def erbah_config = load 'erbah_config.groovy'
 /*def sonarqube_config(node, String branch = null, String properties_file) {
     conf = new Node(null, 'hudson.plugins.sonar.SonarRunnerBuilder', [plugin: 'sonar'])
     if (!properties_file) {
